a6bde45ddb499262964a78befed8cf6bfdbe0789
Raym0NT <konasr6@gmail.com>
2023-03-31 11:33:44 +0300

    dealership page adaptived

--- js/dealership.js
+++ js/dealership.js
@@ -1,4 +1,45 @@
 const ourGoalInviteListSliderour = new Swiper(".our-goal__intive-slider", {
   slidesPerView: 5,
   spaceBetween: 25,
+  breakpoints: {
+    200: {
+      slidesPerView: 1.5,
+      spaceBetween: 10,
+    },
+    400: {
+      slidesPerView: 2.5,
+    },
+    600: {
+      slidesPerView: 3.5,
+      spaceBetween: 25,
+    },
+    1000: {
+      slidesPerView: 4.5,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
 });
+if (window.innerWidth < 900) {
+  const profitableTermsBonusesSlider = new Swiper(
+    ".profitable-terms__bonuses-mobile",
+    {
+      breakpoints: {
+        200: {
+          slidesPerView: 1.5,
+          spaceBetween: 20,
+        },
+        380: {
+          slidesPerView: 2.5,
+
+          spaceBetween: 30,
+        },
+        600: {
+          slidesPerView: 3.5,
+          spaceBetween: 50,
+        },
+      },
+    }
+  );
+}
