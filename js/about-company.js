800fbfe47c4e46f871ac62ed9c36409596397e58
Raym0NT <konasr6@gmail.com>
2023-03-31 19:17:39 +0300

    about-company page completed butwithout adaptive

--- js/about-company.js
+++ js/about-company.js
@@ -1,6 +1,18 @@
 import videoPreviewInit from "./video-preview.js";
 videoPreviewInit();
-const swiper = new Swiper(".history-of-company__activity-images", {
-  slidesPerView: 3,
+const historyOfCompanyImagesSlider = new Swiper(
+  ".history-of-company__activity-images",
+  {
+    slidesPerView: 3,
+    spaceBetween: 40,
+  }
+);
+const teamCardSlider = new Swiper(".team-card__slider", {
+  slidesPerView: 5,
   spaceBetween: 40,
+
+  navigation: {
+    nextEl: ".team-card__slider-arrow--next",
+    prevEl: ".team-card__slider-arrow--prev",
+  },
 });
