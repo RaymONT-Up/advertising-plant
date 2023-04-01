7439e1f7eda21b45ea7f9ecbaf40494c6a4fbf4b
Raym0NT <konasr6@gmail.com>
2023-04-01 10:07:57 +0300

    fixs 2

--- js/dealership.js
+++ js/dealership.js
@@ -1,6 +1,4 @@
 const ourGoalInviteListSliderour = new Swiper(".our-goal__intive-slider", {
-  slidesPerView: 5,
-  spaceBetween: 25,
   breakpoints: {
     200: {
       slidesPerView: 1.5,
@@ -14,31 +12,31 @@ const ourGoalInviteListSliderour = new Swiper(".our-goal__intive-slider", {
       spaceBetween: 25,
     },
     1000: {
-      slidesPerView: 4.5,
+      slidesPerView: 5,
+      spaceBetween: 10,
     },
     1200: {
       slidesPerView: 5,
+      spaceBetween: 25,
     },
   },
 });
-if (window.innerWidth < 900) {
-  const profitableTermsBonusesSlider = new Swiper(
-    ".profitable-terms__bonuses-mobile",
-    {
-      breakpoints: {
-        200: {
-          slidesPerView: 1.5,
-          spaceBetween: 20,
-        },
-        380: {
-          slidesPerView: 2.5,
-          spaceBetween: 30,
-        },
-        600: {
-          slidesPerView: 3.5,
-          spaceBetween: 50,
-        },
+const profitableTermsBonusesSlider = new Swiper(
+  ".profitable-terms__bonuses-mobile",
+  {
+    breakpoints: {
+      200: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
       },
-    }
-  );
-}
+      380: {
+        slidesPerView: 2.5,
+        spaceBetween: 30,
+      },
+      600: {
+        slidesPerView: 3.5,
+        spaceBetween: 50,
+      },
+    },
+  }
+);
