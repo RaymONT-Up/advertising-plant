33d02e2d942b95038a7e67879d4a37ceef701b3e
Raym0NT <konasr6@gmail.com>
2023-04-02 11:00:08 +0300

    franchise page adapted

--- js/franchise.js
+++ js/franchise.js
@@ -7,11 +7,23 @@ const franchiseCompositionSlider = new Swiper(
     slidesPerView: 4.4,
     spaceBetween: 20,
 
-    // breakpoints: {
-    //   200: {
-    //     slidesPerView: 1.3,
-    //   },
-    // },
+    breakpoints: {
+      200: {
+        slidesPerView: 1.1,
+      },
+      360: {
+        slidesPerView: 1.3,
+      },
+      600: {
+        slidesPerView: 2.2,
+      },
+      900: {
+        slidesPerView: 3.4,
+      },
+      1200: {
+        slidesPerView: 4.4,
+      },
+    },
 
     navigation: {
       nextEl: ".franchise-composition__slider-arrow--next",
@@ -22,4 +34,25 @@ const franchiseCompositionSlider = new Swiper(
 const franchiseStepsSlider = new Swiper(".franchise-steps__slider", {
   slidesPerView: 7,
   spaceBetween: 15,
+
+  breakpoints: {
+    200: {
+      slidesPerView: 1.5,
+    },
+    360: {
+      slidesPerView: 2.6,
+    },
+    500: {
+      slidesPerView: 3.5,
+    },
+    715: {
+      slidesPerView: 4.5,
+    },
+    865: {
+      slidesPerView: 5.3,
+    },
+    1100: {
+      slidesPerView: 7,
+    },
+  },
 });
