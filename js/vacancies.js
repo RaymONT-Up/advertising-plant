9a5528836da33ee0cf250585742dbfc6ece7d3a7
Raym0NT <konasr6@gmail.com>
2023-04-18 22:22:24 +0300

    vacancies input type=file fixs

--- js/vacancies.js
+++ js/vacancies.js
@@ -2,3 +2,16 @@ const vacanciesSlider = new Swiper(".career-opportunities__slider", {
   slidesPerView: 5,
   spaceBetween: 20,
 });
+
+const fileInput = document.getElementById("resume-file");
+const customInput = document.querySelector(
+  ".contact-resume__form--custom-file"
+);
+
+fileInput.addEventListener("change", function () {
+  if (fileInput.files.length > 0) {
+    customInput.textContent = fileInput.files[0].name;
+  } else {
+    customInput.textContent = "Прикрепить файл";
+  }
+});
