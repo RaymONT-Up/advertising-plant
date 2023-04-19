8cc11a7bbe60221687e91297beeb176c0acf9110
Raym0NT <konasr6@gmail.com>
2023-04-19 17:31:09 +0300

    vacancies mobile version completed

--- js/vacancies.js
+++ js/vacancies.js
@@ -1,6 +1,31 @@
 const vacanciesSlider = new Swiper(".career-opportunities__slider", {
   slidesPerView: 5,
   spaceBetween: 20,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
+    },
+    300: {
+      slidesPerView: 1.7,
+    },
+    461: {
+      slidesPerView: 2.2,
+    },
+    560: {
+      slidesPerView: 2.7,
+    },
+    700: {
+      slidesPerView: 3.33,
+    },
+    800: {
+      slidesPerView: 3.7,
+    },
+    1000: {
+      slidesPerView: 5,
+    },
+  },
 });
 
 const fileInput = document.getElementById("resume-file");
